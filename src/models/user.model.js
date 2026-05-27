50ab1d32a084a365a73bd6603f1950af16acd08f
KASHISHROY <kashishroy9903@gmail.com>
2026-05-27 18:30:25 +0530

    User schema created

---
+++ src/models/user.model.js
@@ -0,0 +1,34 @@
+const mongoose=require("mongoose")
+const bcrypt=require("bcryptjs")
+
+//userschema with 3 properties.
+const userSchema=mongoose.Schema({
+    email:{
+        type:String,
+        required:[true,"Email is required for creating a user"],
+        trim:true,
+        lowercase:true,
+        match: [ /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Invalid Email address" ],
+        unique: [ true, "Email already exists." ]
+    },
+    name:{
+        type:String,
+        required:[true,"Name is required for creating an account"]
+    },
+    password:{
+        type:String,
+        required:[true,"Password is required for creating an account"],
+        minlength:[6,"password should be containing more than 6 character"],
+        select:false,   //any query to fetch the database password wont come in that query until and unless we specificaaly ask for it
+    }
+},
+    {
+        timestamps:true, //when user was created or updated last time that record
+    }
+)
+
+//whenever you save datato the schema then this function runs->hashing done which is one way can convert plain text to hashed password but not vice versa
+userSchema.pre("save",async function(next)
+{
+
+})
