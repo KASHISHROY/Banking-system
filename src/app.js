6691f01f4540beee0598d93618a828f46ab867a0
KASHISHROY <kashishroy9903@gmail.com>
2026-05-27 18:59:16 +0530

    Register route created

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express=require("express")
 
+const authRouter=require("./routes/auth.routes")
+
 //Instance of server created
 const app=express()
 
+app.use("/api/auth",authRouter) //all the reqstarting from /api/auth will be handled by authRouter
 
 
 
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,9 @@
+const express=require("express")
+
+const router=express.Router()
+
+//first api created is /api/auth/register
+router.post("/register")
+
+
+module.exports=router
